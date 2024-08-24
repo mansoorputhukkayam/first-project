d82ce4fac3a6d03222ac14a1404a6c3ff9880f61
Mansoor <mansoorputhukkayam@gmail.com>
2024-08-25 02:04:47 +0530

    lst

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -64,7 +64,7 @@ const verifyLogin = async (req, res) => {
 
 const loadHome = async (req, res) => {
     try {
-        res.render('adminDashboard');
+        res.render('home');
     } catch (error) {
         console.log(error.message);
         res.status(500).render('error-500');
